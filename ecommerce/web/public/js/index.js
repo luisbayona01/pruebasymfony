20b7453cf20167d6bf0b1f742f3f67f7fa5ef6d4
luisbayona01 <lgbayona1988@gmail.com>
2019-08-09 11:09:25 -0500

    prueba en symfony luis bayona

--- ecommerce/web/public/js/index.js
+++ ecommerce/web/public/js/index.js
@@ -2,7 +2,14 @@ var app = angular.module("myApp", [], function ($interpolateProvider) {
     $interpolateProvider.startSymbol('<%');
     $interpolateProvider.endSymbol('%>');
 })
-var urlglobal = "http://localhost:8000"
+ 
+function getAbsolutePath() {
+    var loc = window.location;
+    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+}
+ var urlglobal =getAbsolutePath(); 
+
 
 app.controller("myCtrl", function ($scope, $http) {
 
